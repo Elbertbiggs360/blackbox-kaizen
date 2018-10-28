f77bedb6a459aa3e71f281069804a2e8aded1fef
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-10-28 05:47:52 +0300

    Add login and getUser methods

--- models/accounts.go
+++ models/accounts.go
@@ -76,3 +76,45 @@ func (account *Account) Create() (map[string] interface{}) {
 	return response
 }
 
+// Login user and return valid auth token
+func Login(email, password string) (map[string]interface{}) {
+
+	account := &Account{}
+	err := GetDB().Table("accounts").Where("email = ?", email).First(account).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return u.Message(false, "Email address not found")
+		}
+		return u.Message(false, "Connection error. Please retry")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(account.Password), []byte(password))
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword { //Password does not match!
+		return u.Message(false, "Invalid login credentials. Please try again")
+	}
+	//Worked! Logged In
+	account.Password = ""
+
+	//Create JWT token
+	tk := &Token{UserID: account.ID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+	account.Token = tokenString //Store the token in the response
+
+	resp := u.Message(true, "Logged In")
+	resp["account"] = account
+	return resp
+}
+
+// GetUser returns the user's account details
+func GetUser(u uint) *Account {
+
+	acc := &Account{}
+	GetDB().Table("accounts").Where("id = ?", u).First(acc)
+	if acc.Email == "" { //User not found!
+		return nil
+	}
+
+	acc.Password = ""
+	return acc
+}
